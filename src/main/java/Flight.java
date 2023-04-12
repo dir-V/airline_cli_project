22af08e495516e584a4990e11222ddc90d140840
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-04-12 09:30:29 +0100

    passenger class completed

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 public class Flight {
     private int iD;
     private String destination;
-    private ArrayList<Passenger>passenger;
+    private ArrayList<Passenger>passengers;
 
-    public Flight(int iD, String destination, ArrayList<Passenger> passenger){
+    public Flight( int iD, String destination, ArrayList<Passenger> passengers){
         this.iD = iD;
         this.destination = destination;
-        this.passenger = new ArrayList<>();
+        this.passengers = new ArrayList<>();
     }
 
-    public int getiD() {
+    public int getId() {
         return iD;
     }
 
-    public void setiD(int iD) {
+    public void setId(int iD) {
         this.iD = iD;
     }
 
@@ -28,14 +28,14 @@ public class Flight {
     }
 
     public ArrayList<Passenger> getPassenger() {
-        return passenger;
+        return passengers;
     }
 
-    public void setPassenger(ArrayList<Passenger> passenger) {
-        this.passenger = passenger;
+    public void setPassenger(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
     }
 //    Method to add new passengers
     public void addPassenger(Passenger passenger){
-        passenger.add(passenger);
+        passengers.add(passenger);
     }
 }
--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,36 @@
 public class Passenger {
+    private String name;
+    private String contactInfo;
+    private int iD;
+
+    public Passenger(String name, String contactInfo, int iD){
+        this.name= name;
+        this.contactInfo= contactInfo;
+        this.iD= iD;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public int getId() {
+        return iD;
+    }
+
+    public void setId(int iD) {
+        this.iD = iD;
+    }
+
 }
