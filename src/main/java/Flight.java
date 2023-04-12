38fdd58ca7a3050558cc95e0689fa528bab581f2
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-04-12 09:22:54 +0100

    flight class

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,4 +1,41 @@
-public class Flight {
-    "yhh";
+import java.util.ArrayList;
 
+public class Flight {
+    private int iD;
+    private String destination;
+    private ArrayList<Passenger>passenger;
+
+    public Flight(int iD, String destination, ArrayList<Passenger> passenger){
+        this.iD = iD;
+        this.destination = destination;
+        this.passenger = new ArrayList<>();
+    }
+
+    public int getiD() {
+        return iD;
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public ArrayList<Passenger> getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(ArrayList<Passenger> passenger) {
+        this.passenger = passenger;
+    }
+//    Method to add new passengers
+    public void addPassenger(Passenger passenger){
+        passenger.add(passenger);
+    }
 }
